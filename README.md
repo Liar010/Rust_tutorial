781170df9ad13563852dbb26d9c2d1244f5bf76d
Liar010 <102400339+Liar010@users.noreply.github.com>
2022-11-14 01:35:29 +0900

    OptionType and ResultType

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Rust_tutorial
 Rustのいろんな本を読んでコードに起こしてみたりサンプルコードを動かしてみたりするためのレポジトリ。
+なお、このレポジトリのコードの動作に責任は持ちません。
 
-A repository for reading various Rust books and trying to convert them into code or to run sample code.(google translate)
-
+(google translate)
+A repository for reading various Rust books and trying to convert them into code or to run sample code.
+Please note that I am not responsible for the behavior of the code in this repository.
--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,23 +1,22 @@
 fn main() {
-    /* if-else fizzbuzz */
-    for i in 1..=100{
-        if (i % 15) == 0 {
-            println!("FizzBuzz");
-        } else if (i % 5) == 0 {
-            println!("Buzz");
-        } else if (i % 3) == 0 {
-            println!("Fizz");
-        } else {
-            println!("{}", i);
+
+    /* Option type : Something or Nothing */
+    let mut word: String = "Hello, World".to_string();
+    for _i in 0..13{/*Recommend use len() method: word.len() */
+        match word.pop() {/* pop(): Removes the last character from the string buffer and returns it. */
+            Some(c) => println!("{}", c),
+            None => println!("String is empty"),
         }
     }
-    /* match fizzbuzz */
-    for i in 1..=100{
-        match i {
-            i if (i % 15) == 0 => println!("FizzBuzz"),
-            i if (i % 5) == 0 => println!("Buzz"),
-            i if (i % 3) == 0 => println!("Fizz"),
-            _ => println!("{}", i),
-        }
+
+    /* Result type : Success(Ok) or failure(Err)*/
+    let four: &str = "4";
+    match four.parse::<u8>() {
+        Ok(n) => println!("parse success: {}", n),
+        Err(err) => println!("{:?}", err),
     }
+    /* or 
+        let num = four.parse::<u8>().unwrap()
+        (if "four" is not number string, unwrap() return "panic" and finish program with error message)
+     */
 }
